2088b8c4bf7197039065a15110ed105d3f7743b3
erickM0 <145382051+erickM0@users.noreply.github.com>
2024-08-13 15:32:28 -0300

    feat: Implementação dos TODOs nas classes filhas

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,21 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    // Herdar da classe "Smartphone"
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, int memoria) : 
+        base( numero, modelo, imei, memoria)
+        {
+            
+        }
+
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            // Sobrescrever o método "InstalarAplicativo"
+            Console.WriteLine("Metodo instalar aplicativo Iphone:\n"+
+                              $"Instalando o app {nomeApp}\n");
+        }
+        
     }
 }
