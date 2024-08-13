17aefde31f1864b3f9580d25910b874198d29e22
erickM0 <145382051+erickM0@users.noreply.github.com>
2024-08-13 15:43:43 -0300

    feat: Testes das classes Nokia e Iphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,34 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("11 9 9999-9999", "modelo 1", "11-12-13-14", 64);
+Iphone iphone= new Iphone("12 9 9999-9990", "Pro Max Plus Advanced", "00-11-22-33",128);
+
+Console.WriteLine("\n\n--teste funcionalidades Nokia--\n\n");
+
+//metodos herdados de Smartphone
+nokia.Ligar();
+nokia.ReceberLigacao();
+
+//metodo sobrescrito pela classe filha Nokia 
+nokia.InstalarAplicativo("WhatsApp");
+
+//metodo para exibir propriedades privadas
+nokia.MostrarConfigurcoes();
+
+Console.WriteLine("\n\n--teste funcionalidades Iphone--\n\n");
+
+//metodos herdados de Smartphone
+iphone.Ligar();
+nokia.ReceberLigacao();
+
+//metodo sobrescrito pela classe filha Nokia 
+iphone.InstalarAplicativo("TikTok");
+
+//metodo para exibir propriedades privadas
+iphone.MostrarConfigurcoes();
+
+
+
+
