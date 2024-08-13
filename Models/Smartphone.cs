21e5a8799e40e944ea97c7f1b4d81429b796f69b
erickM0 <145382051+erickM0@users.noreply.github.com>
2024-08-13 14:53:29 -0300

    feat: propriedades e construtor da classe abstrata Smartphone

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,13 +2,22 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
+        
+        //Implementar as propriedades faltantes de acordo com o diagrama
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo{ get; set; }
+        protected string IMEI{ get; set; }
+        protected int Memoria{ get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
+
+            // Passar os parâmetros do construtor para as propriedades
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+            
         }
 
         public void Ligar()
